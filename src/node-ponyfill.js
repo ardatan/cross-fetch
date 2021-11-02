a1a0a0981fef98eee07a614060d7ec68917551dc
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-02 15:53:26 +0300

    v0.0.11

--- src/node-ponyfill.js
+++ src/node-ponyfill.js
@@ -1,39 +1,39 @@
-const nodeMajor = parseInt(process.versions.node.split('.')[0]);
+const nodeMajor = parseInt(process.versions.node.split('.')[0])
 
 if (nodeMajor >= 16) {
-  const undici = require("undici");
+  const undici = require('undici')
 
-  undici.fetch.ponyfill = true;
+  undici.fetch.ponyfill = true
 
-  module.exports = exports = undici.fetch;
-  exports.fetch = undici.fetch;
-  exports.Headers = undici.Headers;
-  exports.Request = undici.Request;
-  exports.Response = undici.Response;
+  module.exports = exports = undici.fetch
+  exports.fetch = undici.fetch
+  exports.Headers = undici.Headers
+  exports.Request = undici.Request
+  exports.Response = undici.Response
 
   // Needed for TypeScript consumers without esModuleInterop.
-  exports.default = fetch;
+  exports.default = undici.fetch
 } else {
-  const nodeFetch = require("node-fetch");
-  const realFetch = nodeFetch.default || nodeFetch;
+  const nodeFetch = require('node-fetch')
+  const realFetch = nodeFetch.default || nodeFetch
 
   const fetch = function (url, options) {
     // Support schemaless URIs on the server for parity with the browser.
     // Ex: //github.com/ -> https://github.com/
     if (/^\/\//.test(url)) {
-      url = "https:" + url;
+      url = 'https:' + url
     }
-    return realFetch.call(this, url, options);
-  };
+    return realFetch.call(this, url, options)
+  }
 
-  fetch.ponyfill = true;
+  fetch.ponyfill = true
 
-  module.exports = exports = fetch;
-  exports.fetch = fetch;
-  exports.Headers = nodeFetch.Headers;
-  exports.Request = nodeFetch.Request;
-  exports.Response = nodeFetch.Response;
+  module.exports = exports = fetch
+  exports.fetch = fetch
+  exports.Headers = nodeFetch.Headers
+  exports.Request = nodeFetch.Request
+  exports.Response = nodeFetch.Response
 
   // Needed for TypeScript consumers without esModuleInterop.
-  exports.default = fetch;
+  exports.default = fetch
 }
