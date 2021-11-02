96b0d3b85a7ef450c41db3e338292d3766a41b44
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-02 15:48:55 +0300

    Undici

--- src/node-ponyfill.js
+++ src/node-ponyfill.js
@@ -1,22 +1,39 @@
-const nodeFetch = require('node-fetch')
-const realFetch = nodeFetch.default || nodeFetch
+const nodeMajor = parseInt(process.versions.node.split('.')[0]);
 
-const fetch = function (url, options) {
-  // Support schemaless URIs on the server for parity with the browser.
-  // Ex: //github.com/ -> https://github.com/
-  if (/^\/\//.test(url)) {
-    url = 'https:' + url
-  }
-  return realFetch.call(this, url, options)
+if (nodeMajor >= 16) {
+  const undici = require("undici");
+
+  undici.fetch.ponyfill = true;
+
+  module.exports = exports = undici.fetch;
+  exports.fetch = undici.fetch;
+  exports.Headers = undici.Headers;
+  exports.Request = undici.Request;
+  exports.Response = undici.Response;
+
+  // Needed for TypeScript consumers without esModuleInterop.
+  exports.default = fetch;
+} else {
+  const nodeFetch = require("node-fetch");
+  const realFetch = nodeFetch.default || nodeFetch;
+
+  const fetch = function (url, options) {
+    // Support schemaless URIs on the server for parity with the browser.
+    // Ex: //github.com/ -> https://github.com/
+    if (/^\/\//.test(url)) {
+      url = "https:" + url;
+    }
+    return realFetch.call(this, url, options);
+  };
+
+  fetch.ponyfill = true;
+
+  module.exports = exports = fetch;
+  exports.fetch = fetch;
+  exports.Headers = nodeFetch.Headers;
+  exports.Request = nodeFetch.Request;
+  exports.Response = nodeFetch.Response;
+
+  // Needed for TypeScript consumers without esModuleInterop.
+  exports.default = fetch;
 }
-
-fetch.ponyfill = true
-
-module.exports = exports = fetch
-exports.fetch = fetch
-exports.Headers = nodeFetch.Headers
-exports.Request = nodeFetch.Request
-exports.Response = nodeFetch.Response
-
-// Needed for TypeScript consumers without esModuleInterop.
-exports.default = fetch
